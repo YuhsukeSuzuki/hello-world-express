e53f9bd985eeb897896e89b9539c3e1209c54ee7
Ubuntu <yusuke@docker201909.h14f024n2jludk52s10uwmunsb.lx.internal.cloudapp.net>
2019-10-08 05:55:16 +0000

    Add morgan

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var morgan = require('morgan');
+app.use(morgan('combined'));
+	
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Hello World listening on port 3000!'))
+app.listen(3000, () => console.log('Hello World listening on port 3000!'));
